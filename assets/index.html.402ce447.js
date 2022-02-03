9ae90e6fa1afd068e72a26817b25362d276189f5
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-02-03 18:06:57 +0100

    Updates

---
+++ assets/index.html.402ce447.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{$ as e,p as r,H as a}from"./vendor.889ebfc4.js";const i=new e({dependencies:[new r,new a],history:!0});i.initialize();
--- assets/main.dcccd929.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";import{$ as e,M as a,H as r}from"./vendor.1f0ba1e0.js";const i=new e({dependencies:[new a,new r],history:!0});i.initialize();
